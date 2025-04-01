731a0fc662f1e4c76daa9d2773fb815d095d6e3d
Frank Verdi <fverdi@caltech.edu>
2025-04-01 16:48:13 -0700

    Fixed ngps_pipe install

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 import os
 from setuptools import setup
 
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
+# Define version template in case no version control info is available
+VERSION_TEMPLATE = '0.0.0'  # Replace with your desired template version string
+
 try:
     from setuptools_scm import get_version
     version = get_version(root='..', relative_to=__file__)
 except Exception:
-    version = '{version}'
-""".lstrip()
+    version = VERSION_TEMPLATE
 
-setup(use_scm_version={'write_to': os.path.join('ngps_pipe', 'version.py'),
-                       'write_to_template': VERSION_TEMPLATE})
+# Set up the package with version control handling via setuptools_scm
+setup(
+    name='ngps_pipe',  # Your package name
+    version=version,
+    packages=['ngps_pipe'],  # Add your package directory here
+    use_scm_version={
+        'write_to': os.path.join('ngps_pipe', 'version.py'),
+        'write_to_template': VERSION_TEMPLATE
+    },
+    install_requires=[
+        # List of dependencies your package needs
+    ],
+)
